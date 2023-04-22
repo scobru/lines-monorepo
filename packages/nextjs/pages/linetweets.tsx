14698b713776393c6b4f40751f10a31ade712a44
scobru <scobru1988@gmail.com>
2023-04-22 15:28:09 +0200

    fixes UI

--- packages/nextjs/pages/linetweets.tsx
+++ packages/nextjs/pages/linetweets.tsx
@@ -9,6 +9,7 @@ import { toast } from "~~/utils/scaffold-eth";
 import { HeartIcon, ArrowPathIcon, UserPlusIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { formatEther } from "ethers/lib/utils.js";
 import Address from "../components/scaffold-eth/Address";
+import { useRouter } from "next/router";
 
 const LineTweets: NextPage = () => {
   // Define Tweet type
@@ -25,7 +26,7 @@ const LineTweets: NextPage = () => {
   const { data: signer } = useSigner();
   const account = useAccount();
   const provider = useProvider();
-
+  const router = useRouter();
   const [followPrice, setFollowPrice] = React.useState(0);
   const [likePrice, setLikePrice] = React.useState(0);
   const [reTweetPrice, setReTweetPrice] = React.useState(0);
@@ -36,6 +37,8 @@ const LineTweets: NextPage = () => {
   const [followerCount, setFollowerCount] = React.useState(0);
   let ctxAddress!: string;
   let ctxAbi: ContractInterface = [];
+
+  const { id } = router.query;
 
   if (deployedContract) {
     ({ address: ctxAddress, abi: ctxAbi } = deployedContract);
@@ -67,7 +70,9 @@ const LineTweets: NextPage = () => {
   };
 
   useEffect(() => {
-    if (ctx) {
+    if (id) {
+      getAllTweetsForUser(id.toString());
+    } else {
       getContractData();
     }
   }, [ctx]);
@@ -136,93 +141,101 @@ const LineTweets: NextPage = () => {
         <title>L I N E S</title>
         <meta name="description" content="Lines Open Board" />
       </Head>
-      <div className="flex items-center flex-col  w-full justify-center text-left">
-        <div className="collapse">
-          <input type="checkbox" />
-          <div className="collapse-title text-xl font-medium">
-            <div className="text-2xl font-bold my-5 text-center">
-              <button className="btn btn-primary">⚙️ settings</button>
+      <div className="flex mx-auto flex-col  w-3/4 justify-center text-center items-center">
+        <div className="flex flex-grow justify-center">
+          <div className="collapse">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              <div className="text-2xl font-bold my-5 text-center">
+                <button className="btn btn-primary">⚙️ settings</button>
+              </div>
             </div>
-          </div>
-          <div className="collapse-content">
-            <div className="card card-compact bg-primary p-5 shadow-lg shadow-black">
-              <input
-                className="border-2 border-gray-300 bg-gray-300 h-10 mx-auto px-5 py-2 rounded-lg focus:outline-none text-lg text-black"
-                type="text"
-                name="userSearch"
-                placeholder="Search for a user"
-                value={userSearch}
-                onChange={e => setUserSearch(e.target.value)}
-              />
-              <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold my-5  py-2 px-5 rounded "
-                onClick={() => getAllTweetsForUser(userSearch)}
-                disabled={userSearch == ""}
-              >
-                search
-              </button>
-              <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold my-5 py-2 px-5 rounded "
-                onClick={getContractData}
-              >
-                Reset
-              </button>
-              <div className="flex flex-col w-full">
-                <input type="checkbox" id="modal" className="modal-toggle" />
-                <div className="modal">
-                  <div className="modal-box flex flex-col">
-                    <div className="text-2xl font-bold my-5 text-center">Post</div>
-                    <textarea
-                      className="textarea border-2 border-gray-300 bg-gray-300 h-60 mx-auto px-5 rounded-lg focus:outline-none w-full text-lg text-black"
-                      name="message"
-                      placeholder="What's happening?"
-                      value={message}
-                      onChange={e => setMessage(e.target.value)}
-                    />
-                    <button
-                      className="bg-gray-500 hover:bg-gray-700 text-white font-bold my-5 mx-auto py-2 px-5 rounded w-1/4"
-                      onClick={tweet}
-                    >
-                      Send
-                    </button>
+            <div className="collapse-content">
+              <div className="card card-compact bg-primary p-5 shadow-lg shadow-black">
+                <input
+                  className="border-2 border-gray-300 bg-gray-300 h-10 mx-auto px-5 py-2 rounded-lg focus:outline-none text-lg text-black"
+                  type="text"
+                  name="userSearch"
+                  placeholder="Search for a user"
+                  value={userSearch}
+                  onChange={e => setUserSearch(e.target.value)}
+                />
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold my-5  py-2 px-5 rounded "
+                  onClick={() => getAllTweetsForUser(userSearch)}
+                  disabled={userSearch == ""}
+                >
+                  search
+                </button>
+                <button
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold my-5 py-2 px-5 rounded "
+                  onClick={getContractData}
+                >
+                  Reset
+                </button>
+                <div className="flex flex-col w-full">
+                  <input type="checkbox" id="modal" className="modal-toggle" />
+                  <div className="modal">
+                    <div className="modal-box flex flex-col">
+                      <div className="text-2xl font-bold my-5 text-center">Post</div>
+                      <textarea
+                        className="textarea border-2 border-gray-300 bg-gray-300 h-60 mx-auto px-5 rounded-lg focus:outline-none w-full text-lg text-black"
+                        name="message"
+                        placeholder="What's happening?"
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                      />
+                      <button
+                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold my-5 mx-auto py-2 px-5 rounded w-1/4"
+                        onClick={tweet}
+                      >
+                        Send
+                      </button>
 
-                    <div className="modal-action">
-                      <label htmlFor="modal" className="btn">
-                        Close
-                      </label>
+                      <div className="modal-action">
+                        <label htmlFor="modal" className="btn">
+                          Close
+                        </label>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <label htmlFor="modal" className="btn  btn-secondary rounded-lg font-bold w-1/4 mx-auto my-5  ">
-                POST
-              </label>
-              <div className="flex flex-row justify-center items-center">
-                <div className="text-base font-bold my-2 text-center mx-2">
-                  Follow <div className="font-light"> {formatEther(followPrice)} </div>
-                </div>{" "}
-                <div className="text-base font-bold my-2 text-center mx-2">
-                  Like <div className="font-light"> {formatEther(likePrice)} </div>
-                </div>{" "}
-                <div className="text-base font-bold my-2 text-center mx-2">
-                  Repost <div className="font-light"> {formatEther(reTweetPrice)} </div>
-                </div>{" "}
+                <label htmlFor="modal" className="btn  btn-secondary rounded-lg font-bold w-1/4 mx-auto my-5  ">
+                  POST
+                </label>
+                <div className="flex flex-row justify-center items-center">
+                  <div className="text-base font-bold my-2 text-center mx-2">
+                    Follow <div className="font-light"> {formatEther(followPrice)} </div>
+                  </div>{" "}
+                  <div className="text-base font-bold my-2 text-center mx-2">
+                    Like <div className="font-light"> {formatEther(likePrice)} </div>
+                  </div>{" "}
+                  <div className="text-base font-bold my-2 text-center mx-2">
+                    Repost <div className="font-light"> {formatEther(reTweetPrice)} </div>
+                  </div>{" "}
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-left min-w-full text-left">
-          <div className="text-xl font-bold align-text-top items-start justify-start text-left p-10  h-full">
-            <UsersIcon className=" w-8 h-8" />
+        <div className="flex flex-col justify-center items-left w-full text-left">
+          <div className="flex flex-row text-xl font-bold align-text-top items-start justify-start text-left h-full">
+            <UsersIcon className="w-8 h-8" />
             {""} <div className="font-base"> {followerCount} </div>
+            <button className="btn btn-sm bg-primary text-base-content mx-5" onClick={getContractData}>
+              reset
+            </button>
           </div>{" "}
           {listTweet.map((tweet, index) => (
-            <div className="card card-body card-compact card-bordered w-full mx-auto" key={index}>
-              <div className="border-2 border-accent w-full bg-base-300">
+            <div className="card card-compact card-bordered w-full my-5" key={index}>
+              <div className="border-2 border-accent min-w-fit bg-base-300">
                 <div className="flex flex-row">
                   <button
                     className="card-title mx-4 text-base-content text-xl hover:font-bold hover:text-gray-500 justify-start  text-left "
-                    onClick={() => getAllTweetsForUser(tweet.author)}
+                    onClick={() => {
+                      getAllTweetsForUser(tweet.author);
+                      router.push("linetweets?id=" + tweet.author);
+                    }}
                   >
                     <Address address={tweet.author} minimized={false} format="short" disableAddressLink={true} />
                   </button>
@@ -232,7 +245,7 @@ const LineTweets: NextPage = () => {
                         timeZone: "Europe/Rome",
                       })}
                     </p>
-                  </div>
+                  </div>{" "}
                 </div>
                 <div className="flex flex-col">
                   <div className="card-title text-base font-mono mx-4">
